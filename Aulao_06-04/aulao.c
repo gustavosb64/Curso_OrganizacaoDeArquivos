2a8a17286bf067f544228212fa0079505b551a7b
Gustavo <gustavosb64@gmail.com>
2022-04-30 23:47:36 -0300

    Adicionando conteúdo das aulas 01 à 04

---
+++ Aulao_06-04/aulao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+    //fopen
+    //fseek
+    //ftell
+    //fread
+    //fwrite
+    //feof -> NÃO USAR
+    //fclose
+    //hexdump -Cv binario.bin
+
+    return 0;
+}
